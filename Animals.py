975a0869458e9d4cc2e073de65e5acc4924c8941
Calista Mayer <calistammayer@gmail.com>
2023-01-31 21:16:14 -0500

    finished animals

--- Animals.py
+++ Animals.py
@@ -1,37 +1,46 @@
 class Animal:
+    """Animal class"""
+
     def __init__(self, name):
+        """Initializes animal class"""
         self.name = name
 
     def reply(self):
+        """Calls speak method on the object"""
         self.speak()
 
 
 class Mammal(Animal):
+    """Mammal class (methods are on Animal super class)"""
+
     pass
 
 
 class Cat(Mammal):
+    """Cat class"""
+
     def speak(self):
+        """Returns the speak method of cat class"""
         return f"{self.name} says Meow!"
 
 
 class Dog(Mammal):
+    """Dog class, has Mammal super class"""
+
     def speak(self):
+        """Returns the speak method of dog class"""
         return f"{self.name} says Bark!"
 
 
 class Primate(Mammal):
+    """Primate class"""
+
     def speak(self):
+        """Returns the speak method of primate class"""
         return f"Hello, my name is {self.name}."
 
 
 class ComputerScientist(Primate):
+    """Computer Scientist class"""
+
     pass
-
-
-dog = Dog("Bob")
-print(dog.speak())
-computer_scientist = ComputerScientist("Calista")
-print(dir(computer_scientist))
-print(computer_scientist.name)
-print(computer_scientist.speak())
